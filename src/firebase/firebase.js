c0c64ae4da2caf59e61d463e4b576a8978079de4
Unknown <daniel.zimmerman@gmail.com>
2018-08-02 11:59:28 -0600

    Added SignUp Functionality

---
+++ src/firebase/firebase.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyAUetLv1LoJA6d8xS2VME8r8w5wZSH8ttE",
+    authDomain: "react-firebase-authentic-345f1.firebaseapp.com",
+    databaseURL: "https://react-firebase-authentic-345f1.firebaseio.com",
+    projectId: "react-firebase-authentic-345f1",
+    storageBucket: "react-firebase-authentic-345f1.appspot.com",
+    messagingSenderId: "276335862691"
+};
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(config);
+}
+
+const auth = firebase.auth();
+
+export {
+    auth,
+};
