251b6694c8719ccb72eda5680d9fe260b0eb4031
Eric Rohner <ericrohner22@gmail.com>
2018-08-12 14:04:39 -0600

    working profile write/ update and comments

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
-import {Link, withRouter} from 'react-router-dom';
 import AuthUserContext from './AuthUserContext';
-
-import { auth, db } from '../firebase';
-
+import { db } from '../firebase';
 import * as routes from '../constants/routes';
 import withAuthorization from "./withAuthorization";
+import { Link } from 'react-router-dom';
 
-const curProfile = db.onceGetProfile
+//This is what will ultimately be exported, a header, form and div containing the current profile.
+//The last part is broken and not displaying the current profile
 
 const ProfilePage = ({history}) =>
     <div>
@@ -16,22 +15,35 @@ const ProfilePage = ({history}) =>
         <div>{curProfile}</div>
     </div>
 
+//Our initial state
+
 const INITIAL_STATE = {
-    username: '',
     error: null,
     showAuth: false,
     profile: ''
 };
 
+//Our versatile handler to put profile into state as it's typed into the input
+
 const byPropKey = (propertyName, value) => () => ({
     [propertyName]: value,
 });
+
+//Yep, this isn't working
+
+const curProfile = db.onceGetProfile;
+
+//This class contains our profile input form
 
 class ProfileForm extends Component {
     constructor(props) {
         super(props);
         this.state = { ...INITIAL_STATE };
     }
+
+    //There's a little magic going on here. As David explained any <tag> has to live in a return. We need
+    // <AuthUserContext.Consumer> to get user ID so that we can write to db. So we put that code in the return
+    //and have it only execute when showAuth == true
 
     onSubmit = (event) => {
         this.setState({showAuth: true});
@@ -40,30 +52,32 @@ class ProfileForm extends Component {
 
     render() {
 
-        const {
-            profile
-        } = this.state;
-
+        //create a boolean as to whether a profile has been entered to validate whether writing it is a good idea
         const isInvalid =
-            profile === '';
+            this.state.profile === '';
 
         return (
             <div>
             <form onSubmit={this.onSubmit}>
                 <input
-                    value={profile}
+                    value={this.state.profile}
                     onChange={event => this.setState(byPropKey('profile', event.target.value))}
                     type="text"
                     placeholder="Profile goes here"
                 />
+
                 <button disabled={isInvalid} type="submit">
                     Submit
                 </button>
             </form>
+
+                {/*Here's the magic mentioned above, we use <AuthUserContext.Consumer> to get the authUser token
+                and call db.doUpdateProfile. After that setState to initial state to clear the form*/}
+
                 {this.state.showAuth && (
                     <AuthUserContext.Consumer>
                         {authUser =>{
-                            db.doUpdateProfile(authUser.uid, profile)
+                            db.doUpdateProfile(authUser.uid, this.state.profile)
                                 .then(() => {
                             this.setState({ ...INITIAL_STATE });
                                 })
@@ -79,14 +93,22 @@ class ProfileForm extends Component {
     }
 }
 
+//a link to this page to be exported
+
 const ProfileLink = () =>
     <p>
         <Link to={routes.PROFILE}>Profile</Link>
     </p>
 
+//Yeah, no idea what this does
+
 const authCondition = (authUser) => !!authUser;
 
+//Wrap with our higher order component so that we can access authuser
+
 export default withAuthorization(authCondition)(ProfilePage);
+
+//exports for this page and link
 
 export {
     ProfilePage,
--- src/firebase/db.js
+++ src/firebase/db.js
@@ -1,41 +1,71 @@
 import {db} from './firebase';
 
-// User API
+// This is where we write our API, exporting endpoints as consts so that we can keep them all in one place
+
+//Create a new user. We don't need to write an empty attribues [] or profile = '' because firebase won't save
+//that anyway
 
 export const doCreateUser = (id, username, email) => {
-    const attributes = [];
-    const profile = '';
     db.ref(`users/${id}`).set({
         username,
         email,
-        attributes,
-        profile
     });
 }
 
+/*This is called by the component Profile.js to write a profile object
+First test if there is an existing profile for the parentUser
+If the parentUser's profile is null we need to write a new profile object in the DB
+Write that new profile object, and make sure the profile and parentUser both point to each other's keys
+Else, we know they already have a profile so take that profile key and update the corresponding profile*/
+
+//params: parentUser is the db key of the user writing a profile
+//        profile is a string from the Profile.js input form containing profile content
+
 export const doUpdateProfile = async (parentUser, profile) => {
+
+    /*Here we query the db for the profileKey associated with the ParentUser
+    Without async/await the const currentProfile will end up holding a promise. We want the value of that promise
+    and async/await gives us that behavior. There are two options: We get back the db key of the profile if one already
+    exists for this user or null if they haven't made one yet*/
+
     const currentProfile = await db.ref(`users/${parentUser}/profileKey`).once('value')
-        .then(snapshot =>
-    {return( snapshot.val() )})
+        .then(snapshot => {
+                return (
+                    snapshot.val()
+                )
+            }
+        )
+
+    //If currentProfile is null we know that the parentUser hasn't created a profile yet
 
     if (currentProfile == null) {
-        {
-            console.log('profile create')
-        }
-        let profileKey = db.ref().child('profiles').push().key;
+
+        //This line serves two purposes. It pushes a new profile to the DB. Pushing creates a key for us
+        //to reference the new profile by so we store that in const profileKey
+
+        const profileKey = db.ref().child('profiles').push().key;
+
+        //update the parentUser to contain the profileKey so that given a user we can pull its profile from db
+
         db.ref(`users/${parentUser}`).update({
             profileKey
         });
+
+        //Lastly we update the newly made profile with the profile contents and the parentUser so that given a profile
+        //we can access its parentUser from the DB. This will be useful later to pull parentUser attributes for a profile
+
         db.ref(`profiles/${profileKey}`).update({
             profile,
-            parentUser
+            parentUser,
         });
     }
+
+    //currentProfile had a db key in it so update the old profile at that location
+
     else {
         console.log('profile update')
         db.ref(`profiles/${currentProfile}`).update({
-            profile,
-            parentUser
+            profile
         });
     }
 
@@ -45,5 +75,7 @@ export const doUpdateProfile = async (parentUser, profile) => {
 export const onceGetUsers = () =>
     db.ref('users').once('value');
 
-export const onceGetProfile = (id) =>
+//Yeah... this isn't working
+
+export const onceGetProfile =  (id) =>
     db.ref(`profiles/${id}/profile`).once('value');
