2a938b93eef42e549b2c07891e92b45cbc04e795
Unknown <daniel.zimmerman@gmail.com>
2018-08-14 15:36:29 -0600

    Fixed the auth in SignOut.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,7 +11,7 @@ class HomePage extends Component {
 
   async componentDidMount() {
 
-    base.bindToState('users', {
+   await base.bindToState('users', {
       context: this,
       state: 'users',
       withRefs: true,
--- src/components/SignOut.js
+++ src/components/SignOut.js
@@ -3,6 +3,6 @@ import React from 'react'
 import { auth } from '../configuration/firebase'
 
 const SignOutButton = () =>
-  <button type="button" onClick={ auth.doSignOut }>Sign Out</button>
+  <button type="button" onClick= {e => auth.signOut() } >Sign Out</button>
 
 export default SignOutButton
